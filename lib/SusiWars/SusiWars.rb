e3f8c73f1c112387e559e4dc27823a9dda1560f9
veselin <genadiev.veselin@gmail.com>
2014-02-21 00:21:42 +0200

    Added logout request.

--- lib/SusiWars/SusiWars.rb
+++ lib/SusiWars/SusiWars.rb
@@ -15,12 +15,31 @@ class ApplicationServer < Sinatra::Base
     File.read(File.join('public', 'index.html'))
   end
 
+  post '/logout' do
+    if(@request.cookies[:login_key])
+      data = { 'key' => @request.cookies[:login_key] }
+      send_delete(LOGIN_URI, data)
+      @response.delete_cookie(:login_key, nil)
+      redirect to('/')
+    end
+  end
+
   post '/login' do
     login_data = { 'username' => params[:login], 'password' => params[:password] }
     response_key = send_post(LOGIN_URI, login_data)
     key_data = { 'key' => response_key.body }
     response_user_info = send_post(STUDENT_INFO_URI, key_data)
     response_user_info.body
+    @response.set_cookie(:login_key, response_key.body)
+    redirect to('/')
+  end
+
+  def send_delete(uri, data)
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Delete.new(uri.path, {'Content-Type' =>'application/json'})
+    request.body = data.to_json
+    response = http.request(request)
+    response
   end
 
   def send_post(uri, data)
