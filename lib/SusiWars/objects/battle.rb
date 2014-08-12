c94629c8c732a4a2fa2f44781a06c3d04bd5fce7
Veselin-Genadiev <genadiev.veselin@gmail.com>
2014-08-12 22:09:00 +0300

    New beginnings...

--- lib/SusiWars/objects/battle.rb
+++
@@ -1,8 +0,0 @@
-class Battle
-  attr_reader :player_one, :player_two
-
-  def initialize(player_one, player_two)
-    @player_one = player_one
-    @player_two = player_two
-  end
-end
--- lib/SusiWars/version.rb
+++
@@ -1,3 +0,0 @@
-module SusiWars
-  VERSION = "0.0.2"
-end
